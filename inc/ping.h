ab0ad3a4e26a9188815e3d4a77391cdc0095edfe
john smitht <johnsmiththemith@gmail.com>
2025-06-30 16:11:42 +0200

    going to push now, continuing after core completion

--- inc/ping.h
+++ inc/ping.h
@@ -2,8 +2,17 @@
 #define PING_H
 //--* / = \ *-----
 
+#include <cstdint>
 #include <stdio.h>
 
+typedef struct s_ping
+{
+	bool	vebose;
+	bool	help;
+
+	int64_t	ip;
+	char 	*domain;
+} t_ping;
 
 int check_input(int ac, char **av);
 
--- src/main.c
+++ src/main.c
@@ -7,14 +7,21 @@
 * - socket(IPV4, SOCK_RAW, ICMP protocol)
 * */
 
+/* _MODULES_ *
+- PARSING
+- SOCKET COMMUNICATION
+- SIGNAL HANDLING AND TIME TRACKING LOOP (MAIN)
+- STATISTICS AND PRINTING
+*/
+
 int check_input(int ac, char **av)
 {
-
+	if (ac == 1)
 }
 
 int main(int ac, char **av)
 {
-	if ()
+	if (check_input(ac, av))
 
 	return (0);
 }
